7418ae1e8c73962db048c27c07bc6fc6c120d0b6
Nb-coder-894 <nb0183@pleasantonusd.net>
2025-08-04 20:34:11 +0000

    completed game

--- games/rock_paper_scissor/run_rock_paper_scissor.py
+++ games/rock_paper_scissor/run_rock_paper_scissor.py
@@ -1,1 +1,28 @@
 import random
+print("now running rockpaperscissor")
+options = ["rock", "paper", "scissor"]
+while True:
+    player_move = input("Rock, Paper, or Scissor?\n").lower()
+    if player_move not in options:
+        print("Try again.")
+        continue
+    computer_move = random.choice(options)
+    print(f"You chose {player_move}.")
+    print(f"Computer chose {computer_move}.")
+    if player_move == computer_move:
+        print("Tie")
+    elif (
+        (player_move == "rock" and computer_move == "scissor") or
+        (player_move == "scissor" and computer_move == "paper") or
+        (player_move == "paper" and computer_move == "rock")
+    ) :
+      print("You win")
+    else:
+        print("Computer wins")
+    play_again = input("Play again? (y/n)").lower()
+    if play_again != "y":
+        print("bye")
+        break
+    else:
+        continue            
+
